5f5f0728ce89f6647fab547cfadcb5466b9b71ad
pantheepatel <patel.panthee.18@gmail.com>
2025-01-30 18:43:31 +0530

    jquery without netwoek and forms

---
+++ jq_.js
@@ -0,0 +1,116 @@
+$(document).ready(function () {
+
+    // // show-hide
+    // $(".btn1").dblclick(function () {
+    //     $("p1").hide();
+    // });
+    // $("#hide2").mouseenter(function () {
+    //     $(".p2").hide(300); // time to show content fully.
+    // });
+    // $("#show2").mouseleave(function () {
+    //     $(".p2").show("slow");
+    // });
+
+    // $("#showhide3").mousedown(function () {
+    //     $(".p3").toggle(200);
+    // })
+
+    // // fade
+    // $("#showhide3").mouseup(function () {
+    //     $(".p3").fadeOut("fast");
+    // })
+    // $("#showhide3").mousedown(function () {
+    //     $(".p3").fadeToggle(200);
+    // })
+    // $("#showhide3").mousedown(function () {
+    //     $(".p3").fadeTo(200, 0.14);
+    // })
+
+    // // slide
+    // $("#showhide3").mouseup(function () {
+    //     $(".p3").slideDown("fast");
+    // })
+    // $("#showhide3").mousedown(function () {
+    //     $(".p3").slideToggle(200);
+    // })
+    // $("#showhide3").mousedown(function () {
+    //     $(".p3").slideUp(200, function(){
+    //         console.log("slideup")
+    //     });
+    // })
+    // // chaining
+    // $("#p1").css("color", "red").slideUp(2000).slideDown(2000);
+
+
+    // // animation
+    // $("#showhide3").mouseup(function () {
+    //     $(".p3").animate({
+    //         left: '250px',
+    //         opacity: '0.5',
+    //         height: '150px',
+    //         width: '150px'
+    //     });
+    // })
+
+    // // slide-panel-stop-start
+    // $("#panel").hide();
+    // $("#flip").click(function () {
+    //     $("#panel").slideDown(7000);
+    // });
+    // $("#stop").click(function () {
+    //     $("#panel").stop();
+    // });
+
+
+    // // set methods
+    // $("#btn1").click(function () {
+    //     $("#test1").text("Hello world!");
+    // });
+    // $("#btn2").click(function () {
+    //     $("#test2").html("<b>Hello world!</b>");
+    // });
+    // $("#btn3").click(function () {
+    //     $("#test3").val("Dolly Duck");
+    // });
+
+    // $("#btn4").click(function () {
+    //     $("#w3s").attr({
+    //         "href": "https://www.w3schools.com/jquery/",
+    //         "title": "W3Schools jQuery Tutorial"
+    //     });
+    // });
+
+    // // add
+    // $("#p5").prepend("Some prepended text.");
+
+    // // css classes
+    // $("#toggle1").click(function(){
+    //     $("p").toggleClass("class1")
+    // })
+
+    // $("#toggle1").css("color", "pink");
+    // $(".class2").css({ "color": "lightgreen", "background-color": "lightblue" })
+
+    // // set,get height,width
+    // $(".setheight").height("5rem").css("background-color","pink");
+    // $(".setheight").width(200);
+
+    // // traversing
+    // $("#check1").parent().css({ "border": "4px solid pink" })
+    // $("#check1").parents(".div").css({ "border": "4px solid pink" })
+    // $("#check1").parents().css({ "border": "4px solid pink" })
+    // $("#check1").parentsUntil(".div").css({ "border": "4px solid pink" })
+    // $("div").children("div#check1").css({ "border": "4px solid pink" })
+    // $("div").find("*").css({ "border": "4px solid pink" })
+
+});
+
+// if (true) {
+//     let a = 10;
+//     console.log(a++);
+// }
+// console.log(a);
+
+// let a= undefined;
+// console.log(a)
+// console.log(typeof a)
